d9a13ed6210ee62e7af0c116c33b0632987bc6f0
Francisco Ganacim <ganacim@impa.br>
2025-03-04 10:54:06 -0300

    Refactor import statement in test module to use updated resources path

--- mlc/command/test.py
+++ mlc/command/test.py
@@ -1,16 +1,17 @@
+from ..util.resources import data_path
 from .base import Base
-from ..resources import data_path
+
 
 class Test(Base):
     name = "test"
-    
+
     def __init__(self, args):
         super().__init__(args)
 
     @staticmethod
     def add_arguments(parser):
         pass
-        
+
     def run(self):
         print("Running Test command")
         print("data path: ", data_path())
